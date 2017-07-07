a4d66c5091754954bc0fe2a6a0fa376a2409447c
PRO <pro@mbp2017demacbook-pro.local>
2017-07-07 15:50:27 +0800

    ads

---
+++ MyTest2017/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MyTest2017
+//
+//  Created by PRO on 17/7/7.
+//  Copyright © 2017年 BBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ MyTest2017/MainViewController.m
@@ -0,0 +1,42 @@
+//
+//  MainViewController.m
+//  MyTest2017
+//
+//  Created by PRO on 17/7/7.
+//  Copyright © 2017年 BBC. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+ 
+    UILabel * lab  = [UILabel new];
+    [self.view addSubview:lab];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
