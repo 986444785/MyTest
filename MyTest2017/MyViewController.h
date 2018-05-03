b567bb2fe64c3cd54f8c1253943d47bd7d29a0c5
chenweibin <chenweibin@chenweibindeMacBook-Pro.local>
2018-05-03 13:00:56 +0800

    第三次提交测试

---
+++ MyTest2017/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  MyTest2017
+//
+//  Created by chenweibin on 2018/5/3.
+//  Copyright © 2018年 BBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
--- MyTest2017/ViewController.m
+++ MyTest2017/ViewController.m
@@ -18,9 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-//    self.view.backgroundColor =  [UIColor yellowColor];
-    
-    NSLog(@"hello");
+    self.view.backgroundColor =  [UIColor redColor];
     
 }
 
